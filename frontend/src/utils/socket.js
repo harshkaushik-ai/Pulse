231e8f8c75ce21312071082190de2a7e2ddcab54
Harsh Kaushik <harshkaushik096@gmail.com>
2026-05-04 00:35:07 +0530

    changess

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -4,5 +4,8 @@ const socket = io(
   import.meta.env.REACT_APP_API_URL?.replace('/api', '') || 'http://localhost:5000',
   { transports: ['websocket'], reconnection: true, reconnectionDelay: 2000 }
 );
-
+console.log("socket",socket)
+socket.on('connect', () => {
+  console.log('WebSocket connected:', socket.id);
+});
 export default socket;
